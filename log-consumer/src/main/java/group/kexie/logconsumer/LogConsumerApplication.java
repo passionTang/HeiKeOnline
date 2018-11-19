6951b5c0fbb589a6759230ff2c9eda2ba28a708c
tangbo <774044760@qq.com>
2018-11-19 10:36:39 +0800

    重写LogProvider(消息日志发送工具类),解决其他服务消息日志发送时,log-consumer服务接收不到

--- log-consumer/src/main/java/group/kexie/logconsumer/LogConsumerApplication.java
+++ log-consumer/src/main/java/group/kexie/logconsumer/LogConsumerApplication.java
@@ -4,12 +4,16 @@ import group.kexie.logconsumer.messagechannel.LogReceiveChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+
 /*绑定消息通道*/
 @EnableBinding(value = LogReceiveChannel.class)
 @SpringBootApplication
 public class LogConsumerApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(LogConsumerApplication.class, args);
+
     }
 }
--- log-consumer/src/main/java/group/kexie/logconsumer/messageprovide/LogProvider.java
+++ log-consumer/src/main/java/group/kexie/logconsumer/messageprovide/LogProvider.java
@@ -2,25 +2,27 @@ package group.kexie.logconsumer.messageprovide;
 
 import group.kexie.logconsumer.entity.LogMessage;
 import org.apache.log4j.Logger;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
 /**
  * @author tangbo
  * @date 2018/10/19 18:33
- * @description: 消息日志发送类
+ * @description: 消息日志发送工具类
  */
-
 public class LogProvider {
+    /*指定日志消息通道*/
+    @Autowired @Qualifier("channel-log")
+    MessageChannel logChannel;
 
     private static Logger logger = Logger.getLogger(LogProvider.class);
 
-    @SendTo(value = "channel-log")
-    public LogMessage provide(LogMessage logMessage){
+    public void provide(LogMessage logMessage){
         logger.info("发送一个消息日志:"+logMessage);
-        return logMessage;
+
+
+        logChannel.send(MessageBuilder.withPayload(logMessage).build());
     }
 }
